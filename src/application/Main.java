e065144bd07bebc137498eb18485644b3b0937cf
pchenr1que1 <pchenr1que1@gmail.com>
2022-12-21 03:23:41 -0300

    Board and Piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,11 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Main {
   public static void main(String[] args) {
-    Position pos = new Position(3, 5);
+    Board board = new Board(8, 9);
 
-    System.out.println(pos);
+
   }
 }
